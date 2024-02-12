7806dd0a9a923290d97bf1acbde1b88730f4e764
bdamazo266 <143520351+bdamazo266@users.noreply.github.com>
2024-02-12 15:38:28 +0000

    layout.rb

--- dice.rb
+++ dice.rb
@@ -1,6 +1,15 @@
 # /dice.rb
 
 require "sinatra"
-get("/") do
-  "Hello World"
+
+
+get("/dice/2/10") do
+  first_die = rand(1..10)
+  second_die = rand(1..10)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d10</h1>
+   <p>#{outcome}</p>"
 end
